61adbb9f95336a3181886d9c258a32cf0cc9948a
epma4ok-boop <epma4ok@gmail.com>
2026-05-25 10:32:20 +0300

    Delete api/couple/link-test.ts

--- api/couple/link-test.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log("🔵 /api/couple/link-test called");
-  res.status(200).json({ ok: true, message: "link-test works" });
-}

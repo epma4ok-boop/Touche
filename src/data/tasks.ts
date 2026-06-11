a497f321ea4f37e2919b141f2911728bf4b5102c
epma4ok-boop <epma4ok@gmail.com>
2026-06-11 16:31:16 +0300

    Add files via upload

---
+++ src/data/tasks.ts
@@ -0,0 +1,6 @@
+export { TASKS_RU } from "./tasks-ru";
+  export { TASKS_EN } from "./tasks-en";
+  export { TASKS_HI } from "./tasks-hi";
+  export { TASKS_PT } from "./tasks-pt";
+  export { TASKS_ES } from "./tasks-es";
+  
